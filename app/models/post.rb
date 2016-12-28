c8fee1cfb368fd16d8a4b8a1e2f80b4903666e24
Elizabeth Jacobs <elizabethjjacobs@gmail.com>
2016-12-28 13:12:22 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,27 @@
+require 'pry'
+
+
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :clickbait_check
+
+  def clickbait_check
+    final = []
+    clickbait = ["won't believe", "secret", "top", "guess"]
+    clickbait.each do |term|
+      if title != nil
+        if title.downcase.include? (term)
+          final << term
+        end
+      end
+    # binding.pry
+    if final.length == 0
+      errors.add(:title, "not clickbait")
+    end
+  end
+
+end
 end
